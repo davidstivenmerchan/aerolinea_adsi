5607438a8a73c12a695d12d16296d4ac1c599f42
OscarLlanos1412 <70176989+OscarLlanos1412@users.noreply.github.com>
2020-11-30 13:36:46 -0500

    Modifique el registro html y agregue el php

---
+++ php/registro.php
@@ -0,0 +1,34 @@
+<?php
+    require_once("coneccion.php");
+
+    if($_POST['enviar']){
+        //Declaracion de variables para insercion de datos
+        $documento = $_POST['doc'];
+        $nombre = $_POST['nom'];
+        $apel = $_POST['ape'];
+        $direccion = $_POST['dire'];
+        $correo = $_POST['correo'];
+        $contra = $_POST['contra'];
+        $celu = $_POST['celular'];
+        $tip_use = 2;
+        $tip_docu = 1;
+
+        //Hacemos la consulta para insertar los datos en la BD
+        $consulta = "INSERT INTO usuario (documento, nom_user, ape_user, celular, direccion, correo, clave, 
+        id_tip_docu, id_tip_user) VALUES ('$documento', '$nombre', '$apel', '$celu', '$direccion', '$correo', 
+        '$contra', '$tip_docu', '$tip_use')";
+        $sql = mysqli_query($mysqli,$consulta);
+        // $registro = mysqli_fetch_assoc($sql);
+
+        if($sql){
+            echo '<script>alert ("Se agrego correctamente")</script>';
+            header('Location: ../inicio.html');
+        }
+        else{
+            echo "no se agrego"; 
+        }
+    }
+    else{
+        echo "No entro";
+    }
+?>
