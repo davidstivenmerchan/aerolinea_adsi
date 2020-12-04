96cf9847fac9c8643a99d0b8603472697accd2d7
stivenshun <73649327+davidstivenmerchan@users.noreply.github.com>
2020-12-04 18:12:10 -0500

    tabla actualizar funcionando

--- admi/actualizar_Vuelos.php
+++ admi/actualizar_Vuelos.php
@@ -14,7 +14,7 @@ $consulta_vuelos = "SELECT * FROM vuelos WHERE id_vuelo='$id'";
 <title>Panel de edicion de vuelos</title>
 </head>
 <body>
-<form class="tabla_vuelos_administrador" action="">
+<form class="tabla_vuelos_administrador" action="procesar_actualizar_vuelos.php" method="POST">
     <div class="titulo">VUELOS</div>
     <div class="elem_TI">N° VUELO</div>
     <div class="elem_TI">HORA PROGRAMADA</div>
@@ -27,13 +27,14 @@ $consulta_vuelos = "SELECT * FROM vuelos WHERE id_vuelo='$id'";
 <?php $resultados = mysqli_query($mysqli,$consulta_vuelos);
 
 while($row=mysqli_fetch_assoc($resultados)){?>
-    <input type="text" class="elem" value="<?php echo $row["id_vuelo"];?>" style="color:black">
-    <input type="text" class="elem" value="<?php echo $row["hora_programada"];?>" style="color:black">
-    <input type="text" class="elem" value="<?php echo $row["destino"];?>" style="color:black">
-    <input type="text" class="elem" value="<?php echo $row["lugar_salida"];?>" style="color:black">
-    <input type="text" class="elem" value="<?php echo $row["aeropuerto_salida"];?>" style="color:black">
-    <input type="text" class="elem" value="<?php echo $row["estado"];?>" style="color:black">
-    <input type="text" class="elem" value="<?php echo $row["aeropuerto_llegada"];?>" style="color:black">
+    <input type="text" class="elem" value="<?php echo $row["id_vuelo"];?>" style="color:black" name="idvuelo">
+    <input type="text" class="elem" value="<?php echo $row["hora_programada"];?>" style="color:black" name="hora">
+    <input type="text" class="elem" value="<?php echo $row["destino"];?>" style="color:black" name="destino">
+    <input type="text" class="elem" value="<?php echo $row["lugar_salida"];?>" style="color:black" name="lsalida">
+    <input type="text" class="elem" value="<?php echo $row["aeropuerto_salida"];?>" style="color:black" name="asalida">
+    <input type="text" class="elem" value="<?php echo $row["aeropuerto_llegada"];?>" style="color:black" name="estado">
+    <input type="text" class="elem" value="<?php echo $row["estado"];?>" style="color:black" name="allegada">
+    <input type="submit" value="ACTUALIZAR" style="background:rgb(250, 80, 63); cursor:pointer; color:white"  >
 
     <?php } mysqli_free_result($resultados);?>
     
